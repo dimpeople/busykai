043d1cb495cb10972ed0b500dcd0f4351406b4bc
pcstayakl <dimpeople@gmail.com>
2020-06-19 01:29:12 -0400

    initial commit
    initial commit

---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+
+    bool isMoveAllowed;
+    Collider2D collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0) {
+            var touch = Input.GetTouch(0);
+            var touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                var touchedCollider = Physics2D.OverlapPoint(touchPosition);
+                if (collider == touchedCollider)
+                {
+                    isMoveAllowed = true;
+                }
+
+            } else if (touch.phase == TouchPhase.Moved) {
+                if (isMoveAllowed)
+                {
+                    transform.position = new Vector2(touchPosition.x, touchPosition.y);
+                }
+            } else if (touch.phase == TouchPhase.Ended)
+            {
+                isMoveAllowed = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RandomPatrol.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RandomPatrol : MonoBehaviour
+{
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    Vector2 targetPosition;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x >= maxX || transform.position.x <= minX
+            || transform.position.y >= maxY || transform.position.y <= minY) {
+            Reset();
+        } else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        }        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Base")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if (collider.tag == "Floatie")
+        {
+            Reset();
+        }
+    }
+
+    void Reset()
+    {
+        transform.position = RandomBorderPosition();
+        targetPosition = RandomOppositeBorderPosition();
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+    }
+
+    Vector2 RandomBorderPosition()
+    {
+        switch (Random.Range(0,3) / 10)
+        {
+            case 0:
+                return new Vector2(minX, Random.Range(minY, maxY));
+                break;
+            case 1:
+                return new Vector2(maxX, Random.Range(minY, maxY));
+                break;
+            case 2:
+                return new Vector2(Random.Range(minX, maxX), minY);
+                break;
+            case 3:
+                return new Vector2(Random.Range(minX, maxX), maxY);
+                break;
+            default:
+                return new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+                break;
+        }
+    }
+
+    Vector2 RandomOppositeBorderPosition()
+    {
+        if (transform.position.x == minX)
+        {
+            return new Vector2(maxX, Random.Range(minY, maxY));
+        } else if (transform.position.x == maxX)
+        {
+            return new Vector2(minX, Random.Range(minY, maxY));
+        } else if (transform.position.y == minY)
+        {
+            return new Vector2(Random.Range(minX, maxX), maxY);
+        }
+        else 
+        {
+            return new Vector2(Random.Range(minX, maxX), minY);
+        }
+    }
+}
