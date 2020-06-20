24b2d23035e72fae9824010de4aced780350beb5
pcstayakl <dimpeople@gmail.com>
2020-06-20 14:28:29 -0400

    added homeland hit animation

--- Assets/Scripts/Homeland.cs
+++ Assets/Scripts/Homeland.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class Homeland : MonoBehaviour
 {
     public int Health;
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +22,9 @@ public class Homeland : MonoBehaviour
     {
         //Debug.Log("Homeland: took a hit");
         Health -= hit;
+        Debug.Log($"Homeland: Took {hit} damage of {Health}");
+        Debug.Log("Hit animation: Start");
+        animator.SetBool("IsHit", true);
         if (Health <= 0)
         {
             Die();
@@ -29,8 +33,14 @@ public class Homeland : MonoBehaviour
 
     private void Die()
     {
-        //Debug.Log("Homeland: Die");
+        Debug.Log("Homeland: Die");
         Destroy(gameObject);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);        
+    }
+
+    public void OnHitAnimationEnded()
+    {
+        Debug.Log("Hit animation: End");
+        animator.SetBool("IsHit", false);
     }
 }
